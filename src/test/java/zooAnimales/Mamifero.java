9bda862694401defe6bed067804a7656dba8fda8
SofiaGL082 <sogiraldo@unal.edu.co>
2023-09-21 22:07:37 -0500

    :+1:

--- src/test/java/zooAnimales/Mamifero.java
+++ src/test/java/zooAnimales/Mamifero.java
@@ -7,8 +7,8 @@ public class Mamifero extends Animal{
 	private static List<Mamifero> listado = new ArrayList<Mamifero>();
 	public static int caballos = 0;
 	public static int leones = 0;
-	private boolean pelaje;
-	private int patas;
+	private static boolean pelaje;
+	private static int patas;
 	
 	//Constructor vacio
 	public Mamifero() {
@@ -18,22 +18,22 @@ public class Mamifero extends Animal{
 	//Constructor atributos
 	public Mamifero(String nombre, int edad, String habitat, String genero, boolean pelaje, int patas) {
 		super(nombre, edad, habitat, genero);
-		this.pelaje = pelaje;
-		this.patas = patas;
+		Mamifero.pelaje = pelaje;
+		Mamifero.patas = patas;
 		listado.add(this);
 	}
 	
 	//Metodos get y set para atributos privados
 	
 	public void setPelaje(boolean pelaje) {
-		this.pelaje = pelaje;
+		Mamifero.pelaje = pelaje;
 	}
 	public boolean getPelaje() {
 		return pelaje;
 	}
 	
 	public void setPatas(int patas) {
-		this.patas = patas;
+		Mamifero.patas = patas;
 	}
 	public int getPatas() {
 		return patas;
@@ -43,26 +43,24 @@ public class Mamifero extends Animal{
 	public static int cantidadMamiferos() {
 		return listado.size();
 	}
-	public void crearCaballo(String nombre, int edad, String genero) {
-		super.setNombre(nombre);
-		super.setEdad(edad);
-		super.setHabitat("pradera");
-		super.setGenero(genero);
-		this.pelaje = true;
-		this.patas = 4; 
+	public static void crearCaballo(String nombre, int edad, String genero) {
+		Animal.setNombre(nombre);
+		Animal.setEdad(edad);
+		Animal.setHabitat("pradera");
+		Animal.setGenero(genero);
+		Mamifero.pelaje = true;
+		Mamifero.patas = 4; 
 		caballos++;
-		listado.add(this);
 		
 	}
-	public void crearLeon(String nombre, int edad, String genero) {
-		super.setNombre(nombre);
-		super.setEdad(edad);
-		super.setHabitat("selva");
-		super.setGenero(genero);
-		this.pelaje = true;
-		this.patas = 4;
+	public static void crearLeon(String nombre, int edad, String genero) {
+		Animal.setNombre(nombre);
+		Animal.setEdad(edad);
+		Animal.setHabitat("selva");
+		Animal.setGenero(genero);
+		Mamifero.pelaje = true;
+		Mamifero.patas = 4;
 		leones++;
-		listado.add(this);
 	}
 
 }
